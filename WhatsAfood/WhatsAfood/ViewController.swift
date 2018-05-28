bb82474352d101071fda873451f522d8b8e81b8a
yu xu <yuxu@yus-macbook-pro.local>
2018-05-27 19:19:20 -0700

    initial commit

---
+++ WhatsAfood/WhatsAfood/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  WhatsAfood
+//
+//  Created by yu xu on 5/27/18.
+//  Copyright © 2018 yu xu. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var imageView_Camera: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView_Camera.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
