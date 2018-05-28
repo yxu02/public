f362a07edd79c4982f52a94372a7d62b5c76f7c8
yu xu <yuxu@yus-macbook-pro.local>
2018-05-27 21:13:32 -0700

    2nd commit. Added SVProgressHUD

--- WhatsAfood/WhatsAfood/ViewController.swift
+++ WhatsAfood/WhatsAfood/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreML
 import Vision
+import SVProgressHUD
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
@@ -23,17 +24,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
     }
-    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        viewWillAppear(true)
+    }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView_Camera.image = image
+            SVProgressHUD.show()
+            guard let ciimage = CIImage(image: image) else {
+                fatalError("error at converting uiimage to ciimage")
+            }
+            detect(ciimage)
         }
-        
+        SVProgressHUD.dismiss()
         picker.dismiss(animated: true, completion: nil)
+        
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(_ image: CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("error at retrieving ML model")
+        }
+        let requests = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("error at retrieving observation results")
+            }
+            guard let firstResult = results.first else{
+                fatalError("no result was found")
+            }
+            let item = firstResult.identifier
+            self.navigationItem.title = item
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([requests])
+        }catch{
+            print("error at processing: \(error)")
+        }
+        
     }
     
 }
