0373a5dfd84e3efb51e4629d4962f976a09c44c7
yu xu <yuxu@yus-MacBook-Pro.local>
2018-05-29 00:08:30 -0700

    FlowerDx app first commit

---
+++ FlowerDx/FlowerDx/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  FlowerDx
+//
+//  Created by yu xu on 5/28/18.
+//  Copyright © 2018 yu xu. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    let WIKI_API = "https://en.wikipedia.org/w/api.php"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        SVProgressHUD.show()
+        cameraButton.isEnabled = false
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else{
+            fatalError("Error at getting UI image")
+        }
+        imageView.image = image
+        guard let ciimage = CIImage(image: image) else{
+            fatalError("Error at getting CIImage")
+        }
+        
+        detect(image: ciimage)
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage){
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else{
+            fatalError("Error at retrieving ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Error at retrieving model result")
+            }
+            guard let firstResult = results.first else{
+                fatalError("no result was found")
+            }
+            let item = firstResult.identifier
+            self.navigationItem.title = item
+            
+            DispatchQueue.main.async {
+                self.cameraButton.isEnabled = true
+                SVProgressHUD.dismiss()
+            }
+            self.callWikiAPI(mlResultName: item)
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+        }catch{
+            print("Error at handling image process request with the error: \(error)")
+        }
+    }
+    
+    func callWikiAPI(mlResultName : String){
+        let params: [String: String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : mlResultName,
+            "indexpageids" : "",
+            "redirects" : "1"
+        ]
+        Alamofire.request(WIKI_API, method: .get, parameters: params).responseJSON {
+            (response) in
+            if response.result.isSuccess{
+                let wikiJSON: JSON = JSON(response.result.value!)
+                print(wikiJSON)
+            }
+            if response.result.isFailure{
+                print("Error: \(String(describing: response.result.error))")
+            }
+        }
+    }
+}
+
