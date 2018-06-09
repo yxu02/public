e1fd8194a5f189705db55ee75ce84c057dd8aa5d
yxu02 <31361743+yxu02@users.noreply.github.com>
2018-06-08 19:42:32 -0700

    initial commit

---
+++ FirstAR/FirstAR/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  FirstAR
+//
+//  Created by yu xu on 5/29/18.
+//  Copyright © 2018 yu xu. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        print("WorldTrackingSession is supported: \(ARWorldTrackingConfiguration.isSupported)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
