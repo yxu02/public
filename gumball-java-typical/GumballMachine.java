447af6b11a35e54e8c878d8f82db99928a66f7f8
yuxu <yzx105psu@gmail.com>
2018-06-12 15:59:57 -0700

    final commit

--- gumball-java-typical/GumballMachine.java
+++ gumball-java-typical/GumballMachine.java
@@ -3,41 +3,42 @@ public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    boolean has_money;
 
     public GumballMachine( int size )
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        this.has_money = false;
     }
 
     public void insertQuarter(int coin)
     {
         if ( coin == 25 )
-            this.has_quarter = true ;
+            this.has_money = true ;
         else 
-            this.has_quarter = false ;
+            this.has_money = false ;
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
+    	if ( this.has_money)
     	{
     		if ( this.num_gumballs > 0 )
     		{
     			this.num_gumballs-- ;
-    			this.has_quarter = false ;
+    			this.has_money = false ;
     			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+                System.out.println("Remaining balls: " + this.num_gumballs);
     		}
     		else
     		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+    			System.out.println( "No More Gumballs!  Sorry, can't return your money." ) ;
     		}
     	}
     	else 
     	{
-    		System.out.println( "Please insert a quarter" ) ;
+    		System.out.println( "Please insert money." ) ;
     	}        
     }
 }
